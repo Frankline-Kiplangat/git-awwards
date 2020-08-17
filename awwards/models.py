e4ef33f10cc06a2c30fc5a58100a8763643322bb
Frank <kipfrankline@gmail.com>
2020-08-17 20:33:25 +0300

    Create model class containing project objects

--- awwards/models.py
+++ awwards/models.py
@@ -4,3 +4,11 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Profile(models.Model):
+    """
+    class containing projects' objects
+    """
+    user = models.OneToOneField(User, on_delete=models.CASCADE, default=None, null=True, blank=True)
+    profile_pic = models.ImageField( default = 'default.jpg',upload_to='profilepics/')
+    bio = models.TextField(max_length=80, blank=True)
+    contact = models.CharField(max_length =200,blank=True)
