79ccc57c537e0b613fd12ddfcc5c4fd813e2c07d
Frank <kipfrankline@gmail.com>
2020-08-17 13:46:19 +0300

    Add  function that renders user's profile

--- awwards/views.py
+++ awwards/views.py
@@ -59,3 +59,29 @@ def index(request):
     all_projects = Projects.objects.all()
     return render(request, 'index.html', {'all_projects':all_projects})
 
+@login_required(login_url='login')
+def profile(request):
+    """
+    view function that renders user's profile
+    """
+    current_user = request.user
+    if request.method == 'POST':
+        u_form = UserUpdateForm(request.POST,instance=request.user)
+        p_form = ProfileUpdateForm(request.POST,request.FILES,instance=request.user.profile)
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save() 
+            messages.success(request, f'Your account has been updated!')
+            return redirect('profile')
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm(instance=request.user.profile)
+        my_projects = Projects.objects.filter(project_user=current_user)
+        
+    
+    context = {
+        'u_form': u_form,
+        'p_form': p_form,
+        'my_projects':my_projects    
+    }
+    return render(request, 'profile.html', context)
