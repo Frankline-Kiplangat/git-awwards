a0865525b423c761fd39f4b8cca0e6f572abdb43
Frank <kipfrankline@gmail.com>
2020-08-17 20:23:42 +0300

    Register admin models

--- awwards/admin.py
+++ awwards/admin.py
@@ -2,5 +2,8 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
-
+from .models import Profile, Projects, Events
 # Register your models here.
+admin.site.register(Projects)
+admin.site.register(Events)
+admin.site.register(Profile)
