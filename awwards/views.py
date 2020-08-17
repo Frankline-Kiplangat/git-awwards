c82ecb8774bb37c638d5120772e5d7fafd1c5911
Frank <kipfrankline@gmail.com>
2020-08-17 13:54:56 +0300

    Add function to register a new user

--- awwards/views.py
+++ awwards/views.py
@@ -8,6 +8,21 @@ from django.contrib import messages
 from .forms import UserRegisterForm,UserUpdateForm,ProfileUpdateForm,UploadForm
 
 # Create your views here.
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Your account has been created! You are now able to login!')
+            return redirect('login')
+    else:
+        form = UserRegisterForm()
+    return render(request, 'registration/register.html', {'form':form})
+
+
+
 def search_results(request):
     """
     view function that returns the searched projects
